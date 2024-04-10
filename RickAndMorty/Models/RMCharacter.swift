853971356395c96b34ff9d5961532c9d75c50e03
Pavel Bohomolnyi <pavel.bohomolnyi@skopei.com>
2024-04-10 13:33:27 +0200

    Add some basic model data. Correct commit

--- RickAndMorty/Models/RMCharacter.swift
+++ RickAndMorty/Models/RMCharacter.swift
@@ -1,8 +1,24 @@
 //
-//  Character.swift
+//  RMCharacter.swift
 //  RickAndMorty
 //
 //  Created by Pavel Bohomolnyi on 25/12/2023.
 //
 
 import Foundation
+
+struct RMCharacter: Codable {
+    let id: Int
+    let name: String
+    let status: RMCharacterStatus
+    let species: String
+    let type: String
+    let gender: RMCharacterGender
+    let origin: RMOrigin
+    let location: RMSingleLocation
+    let image: String
+    let episode: [String]
+    let url: String
+    let created: String
+}
+
--- RickAndMorty/Models/RMLocation.swift
+++ RickAndMorty/Models/RMLocation.swift
@@ -1,9 +1,19 @@
 //
-//  Location.swift
+//  RMLocation.swift
 //  RickAndMorty
 //
 //  Created by Pavel Bohomolnyi on 25/12/2023.
 //
 
 import Foundation
+
+struct RMLocation: Codable {
+    let id: Int
+    let name: String
+    let type: String
+    let dimension: String
+    let residents: [String]
+    let url: String
+    let created: String
+}
 
