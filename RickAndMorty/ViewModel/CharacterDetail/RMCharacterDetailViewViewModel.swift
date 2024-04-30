c72118b67a1d3604f1a0242444e72bd3fa4d3f6a
Pavel Bohomolnyi <pavel.bohomolnyi@skopei.com>
2024-04-30 11:51:19 +0200

    Character Info ViewModel

--- RickAndMorty/ViewModel/CharacterDetail/RMCharacterDetailViewViewModel.swift
+++ RickAndMorty/ViewModel/CharacterDetail/RMCharacterDetailViewViewModel.swift
@@ -32,14 +32,14 @@ final class RMCharacterDetailViewViewModel {
         sections = [
             .photo(viewModel: .init(imageUrl: URL(string: character.image))),
             .information(viewModel: [
-                .init(value: character.status.text, title: "Status"),
-                .init(value: character.gender.rawValue, title: "Gender"),
-                .init(value: character.species, title: "Type"),
-                .init(value: character.species, title: "Species"),
-                .init(value: character.origin.name, title: "Origin"),
-                .init(value: character.location.name, title: "Location"),
-                .init(value: character.created, title: "Created"),
-                .init(value: "\(character.episode.count)", title: "Total Episodes")
+                .init(type: .status, value: character.status.text),
+                .init(type: .gender, value: character.gender.rawValue),
+                .init(type: .type, value: character.species),
+                .init(type: .species, value: character.species),
+                .init(type: .origin, value: character.origin.name),
+                .init(type: .location, value: character.location.name),
+                .init(type: .created, value: character.created),
+                .init(type: .episodeCount, value: "\(character.episode.count)")
             ]),
             .episodes(viewModel: character.episode.compactMap {
                 return RMCharacterEpisodeCollectionViewCellViewModel(episodeDataUrl: URL(string: $0))
